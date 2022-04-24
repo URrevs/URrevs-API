49cfc0a740bb59afa376bd106048a437b795412f
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-04-25 00:14:12 +0200

    setting cors

--- urrevs/routes/index.js
+++ urrevs/routes/index.js
@@ -6,6 +6,9 @@
 var express = require('express');
 var router = express.Router();
 
+const cors = require("../utils/cors");
+router.use(cors.cors);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({"status": "deployed successfully"});
