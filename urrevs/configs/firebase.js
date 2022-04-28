78f969c5a432366bc665f7bc81c6023d3b6825a3
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-04-28 02:49:32 +0200

    copyrights

--- urrevs/configs/firebase.js
+++ urrevs/configs/firebase.js
@@ -1,3 +1,8 @@
+/*
+  Author: Abdelrahman Hany
+  Created on: 8-Apr-2022
+*/
+
 const admin = require("firebase-admin");
 const config = require("../config")
 const serviceAccount = require(process.env.FIREBASE_ADMIN_KEY || config.FIREBASE_ADMIN_KEY);
