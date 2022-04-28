8f358f853e4437c6a027842975c99bb16a435f86
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-04-28 15:18:50 +0200

    search phones only

---
+++ urrevs/routes/searchRouter.js
@@ -0,0 +1,54 @@
+/*
+  Author: Abdelrahman Hany
+  Created on: 28-Apr-2022
+*/
+
+const express = require("express");
+
+const cors = require("../utils/cors");
+
+const PHONE = require("../models/phone");
+const phone = require("../models/phone");
+
+const searchRouter = express.Router();
+
+// Applying important middlewares
+searchRouter.use(cors.cors);
+
+//--------------------------------------------------------------------
+
+// Endpoints Implementation
+
+
+searchRouter.get("/products/phones", (req, res, next)=>{
+  let searchWord = req.query.q;
+  if(!searchWord){
+    res.statusCode = 400;
+    res.setHeader("Content-Type", "application/json");
+    res.json({success: false, status: "bad request"});
+    return;
+  }
+
+  PHONE.find({name: {$regex: searchWord, $options: "i"}}, {name: 1}).limit(5).then((phones)=>{
+    let result = [];
+    for(p of phones){
+      result.push({
+        _id: p._id,
+        name: p.name,
+        type: "phone"
+      });
+    }
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({success: true, phones: result});
+  }).
+  catch((err)=>{
+    console.log("Error from /products/phones: ", err);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.json({success: false, status: "process failed"});
+  })
+});
+
+
+module.exports = searchRouter;
