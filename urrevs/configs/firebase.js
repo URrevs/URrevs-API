6416cce93c2ad3e1aca8ffd0d36776b83f16a87f
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-04-28 02:47:09 +0200

    avoiding ci cd error when requiring the path of the service acc

--- urrevs/configs/firebase.js
+++ urrevs/configs/firebase.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.FIREBASE_ADMIN_KEY);
+const config = require("../config")
+const serviceAccount = require(process.env.FIREBASE_ADMIN_KEY || config.FIREBASE_ADMIN_KEY);
 
 module.exports = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
