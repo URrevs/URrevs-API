a027499a83d13fcc06a87b6d368971cd33709e91
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-04-29 08:24:02 +0200

    get phones from a specific company

--- urrevs/routes/phoneRouter.js
+++ urrevs/routes/phoneRouter.js
@@ -54,4 +54,41 @@ phoneRouter.get("/all", rateLimit.regular, cors.cors, (req, res, next) => {
 
 
 
+// list all phones from a specific company
+phoneRouter.get("/by/:compId", rateLimit.regular, cors.cors, (req, res, next)=>{
+    let itemsPerRound = parseInt((process.env.PHONES_BY_COMPANY_PER_ROUND|| config.PHONES_BY_COMPANY_PER_ROUND));
+    let roundNum = req.query.round;
+    if(!roundNum){
+        res.statusCode = 400;
+        res.setHeader("Content-Type", "application/json");
+        res.json({success: false, status: "bad request"});
+        return;
+    }
+
+    PHONE.find({company: req.params.compId})
+    .sort({createdAt: -1})
+    .skip((roundNum - 1) * itemsPerRound)
+    .limit(itemsPerRound)
+    .then((phones) => {
+        let result = [];
+        for(let phone of phones){
+            result.push({
+                _id: phone._id,
+                name: phone.name,
+                type: "phone"
+            });
+        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json({success: true, phones: result});
+    })
+    .catch((err)=>{
+        console.log("Error from /phones/by/:compId: ", err);
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({success: false, status: "process failed"});
+    })
+});
+
+
 module.exports = phoneRouter;
