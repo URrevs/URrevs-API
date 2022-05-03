b3c6da0b4360f27bdf3ed6781c67fed22f35b80a
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-05-03 21:51:50 +0200

    modifying models names - creating reviews unlikes model

---
+++ urrevs/models/companyRevsUnlikes.js
@@ -0,0 +1,26 @@
+/*
+  Author: Abdelrahman Hany
+  Created on: 3-May-2022
+*/
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const revsUnlikesSchema = new schema({
+    user: {
+        type: schema.Types.ObjectId,
+        ref: 'User'
+    },
+    review: {
+        type: schema.Types.ObjectId,
+        ref: 'cRev'
+    }
+},
+{
+    timestamps: true
+});
+
+
+revsUnlikesSchema.index({createdAt: 1});
+
+mongoose.model('cRevsUnlike', revsUnlikesSchema);
