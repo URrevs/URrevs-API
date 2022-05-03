b4f819e7f4efe14bb8ba1a6703ed45a348b1da51
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-05-03 19:16:51 +0200

    round num must be a number

--- urrevs/models/phoneReview.js
+++ urrevs/models/phoneReview.js
@@ -83,7 +83,7 @@ const pReviewSchema = new schema({
 });
 
 pReviewSchema.index({createdAt: -1});
-pReviewSchema.index({phone: 1, createdAt: -1});
-pReviewSchema.index({user: 1, createdAt: -1});
+pReviewSchema.index({phone: 1, likes: -1, createdAt: -1});
+pReviewSchema.index({user: 1, likes: -1, createdAt: -1});
 
 module.exports = mongoose.model("pReview", pReviewSchema);
