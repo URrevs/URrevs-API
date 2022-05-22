3bf50b49e6b25e52b1b2e69e8fb12802318facb8
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-05-22 13:47:15 +0200

    simpler looking code

--- urrevs/scheduled/currencyConversion.js
+++ urrevs/scheduled/currencyConversion.js
@@ -14,82 +14,109 @@ const delay = (milliseconds)=>{
 
 
 module.exports = ()=>{
-    cron.schedule(process.env.CURRENCY_UPDATE_SCHEDULE_PATTERN , ()=>{  // Every day at 3:00 AM
+    cron.schedule(process.env.CURRENCY_UPDATE_SCHEDULE_PATTERN , async()=>{  // Every day at 3:00 AM
         // Get latest conversions from EUR to EGP
         let TIMEOUT = parseInt(process.env.TIMEOUT) || config.TIMEOUT;
-        let rates, oneEur, oneEgp, eurToEgp;
+        let rates, oneEur, oneCurr, eurToCurr;
         
         // convert from EUR to EGP
-        axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"EGP,EUR"}}, 
-        {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })}).then(({data: exRates})=>{
+        try{
+            let {data: exRates} = await axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"EGP,EUR"}}, 
+                {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })});
             rates = exRates.rates;
             oneEur = rates.EUR;
-            oneEgp = rates.EGP;
-            eurToEgp = oneEgp / oneEur;
+            oneCurr = rates.EGP;
+            eurToCurr = oneCurr / oneEur;
 
-            CONSTANT.findOneAndUpdate({name: "EURToEGP"}, [{$set: {value: eurToEgp.toString(), date: new Date()}}], {upsert: true}).then(async()=>{
+            // update the conversion rate in the database
+            try{
+                await CONSTANT.findOneAndUpdate({name: "EURToEGP"}, [{$set: {value: eurToCurr.toString(), date: new Date()}}], {upsert: true});
                 console.log("End of scheduled EUR to EGP currency update (SUCCESS)..........................");
-                await delay(1000);
-                // convert from INR to EUR
-                axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"INR,EUR"}}, 
-                {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })}).then(({data: exRates})=>{
-                    rates = exRates.rates;
-                    oneEur = rates.EUR;
-                    oneEgp = rates.INR;
-                    eurToEgp = oneEur / oneEgp;
-        
-                    CONSTANT.findOneAndUpdate({name: "INRToEUR"}, [{$set: {value: eurToEgp.toString(), date: new Date()}}], {upsert: true}).then(async()=>{
-                        console.log("End of scheduled INR to EUR currency update (SUCCESS)..........................");
-                        await delay(1000);
-                        // convert from GBP to EUR
-                        axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"GBP,EUR"}}, 
-                        {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })}).then(({data: exRates})=>{
-                            rates = exRates.rates;
-                            oneEur = rates.EUR;
-                            oneEgp = rates.GBP;
-                            eurToEgp = oneEur / oneEgp;
-                
-                            CONSTANT.findOneAndUpdate({name: "GBPToEUR"}, [{$set: {value: eurToEgp.toString(), date: new Date()}}], {upsert: true}).then(async()=>{
-                                console.log("End of scheduled GBP to EUR currency update (SUCCESS)..........................");
-                                await delay(1000);
-                                // convert from USD to EUR
-                                axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"USD,EUR"}}, 
-                                {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })}).then(({data: exRates})=>{
-                                    rates = exRates.rates;
-                                    oneEur = rates.EUR;
-                                    oneEgp = rates.USD;
-                                    eurToEgp = oneEur / oneEgp;
-                        
-                                    CONSTANT.findOneAndUpdate({name: "USDToEUR"}, [{$set: {value: eurToEgp.toString(), date: new Date()}}], {upsert: true}).then(()=>{
-                                        console.log("End of scheduled USD to EUR currency update (SUCCESS)..........................");
-                                    })
-                                    .catch((e)=>{
-                                        console.log("End of scheduled USD to EUR currency update (FAILURE)..........................");
-                                    });
-                                }).catch((e)=>{
-                                    console.log("End of scheduled USD to EUR currency update (FAILURE)..........................");
-                                });
-
-                            })
-                            .catch((e)=>{
-                                console.log("End of scheduled GBP to EUR currency update (FAILURE)..........................");
-                            });
-                        }).catch((e)=>{
-                            console.log("End of scheduled GBP to EUR currency update (FAILURE)..........................");
-                        });
-                    })
-                    .catch((e)=>{
-                        console.log("End of scheduled INR to EUR currency update (FAILURE)..........................");
-                    });
-                }).catch((e)=>{
-                    console.log("End of scheduled INR to EUR currency update (FAILURE)..........................");
-                });
-            })
-            .catch((e)=>{
+            }
+            catch(err){
                 console.log("End of scheduled EUR to EGP currency update (FAILURE)..........................");
-            });
-        }).catch((e)=>{
+            }
+        }
+        catch(err){
             console.log("End of scheduled EUR to EGP currency update (FAILURE)..........................");
-        });
+        }
+
+        //-------------------------------------------------------------------------------------
+        await delay(1000);
+        //-------------------------------------------------------------------------------------
+
+        // convert from INR to EUR
+        try{
+            let {data: exRates} = await axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"INR,EUR"}}, 
+                {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })});
+            rates = exRates.rates;
+            oneEur = rates.EUR;
+            oneCurr = rates.INR;
+            eurToCurr = oneEur / oneCurr;
+
+            // update the conversion rate in the database
+            try{
+                await CONSTANT.findOneAndUpdate({name: "INRToEUR"}, [{$set: {value: eurToCurr.toString(), date: new Date()}}], {upsert: true});
+                console.log("End of scheduled INR to EUR currency update (SUCCESS)..........................");
+            }
+            catch(err){
+                console.log("End of scheduled INR to EUR currency update (FAILURE)..........................");
+            }
+        }
+        catch(err){
+            console.log("End of scheduled INR to EUR currency update (FAILURE)..........................");
+        }
+
+        //-------------------------------------------------------------------------------------
+        await delay(1000);
+        //-------------------------------------------------------------------------------------
+
+        // convert from GBP to EUR
+        try{
+            let {data: exRates} = await axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"GBP,EUR"}}, 
+                        {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })});
+            rates = exRates.rates;
+            oneEur = rates.EUR;
+            oneCurr = rates.GBP;
+            eurToCurr = oneEur / oneCurr;
+
+            // update the conversion rate in the database
+            try{
+                await CONSTANT.findOneAndUpdate({name: "GBPToEUR"}, [{$set: {value: eurToCurr.toString(), date: new Date()}}], {upsert: true});
+                console.log("End of scheduled GBP to EUR currency update (SUCCESS)..........................");
+            }
+            catch(err){
+                console.log("End of scheduled GBP to EUR currency update (FAILURE)..........................");
+            }
+        }
+        catch(err){
+            console.log("End of scheduled GBP to EUR currency update (FAILURE)..........................");
+        }
+
+        //-------------------------------------------------------------------------------------
+        await delay(1000);
+        //-------------------------------------------------------------------------------------
+
+        // convert from USD to EUR
+        try{
+            let {data: exRates} = await axios.get(config.EXCHANGE_RATES_API+"/latest", {params: {access_key: (process.env.EXCHANGE_RATES_ACCESS_KEY2), symbols:"USD,EUR"}}, 
+                                {timeout: TIMEOUT, httpsAgent: new https.Agent({ keepAlive: true })});
+            rates = exRates.rates;
+            oneEur = rates.EUR;
+            oneCurr = rates.USD;
+            eurToCurr = oneEur / oneCurr;
+
+            // update the conversion rate in the database
+            try{
+                await CONSTANT.findOneAndUpdate({name: "USDToEUR"}, [{$set: {value: eurToCurr.toString(), date: new Date()}}], {upsert: true});
+                console.log("End of scheduled USD to EUR currency update (SUCCESS)..........................");
+            }
+            catch(err){
+                console.log("End of scheduled USD to EUR currency update (FAILURE)..........................");
+            }
+        }
+        catch(err){
+            console.log("End of scheduled USD to EUR currency update (FAILURE)..........................");
+        }
     });
 };
