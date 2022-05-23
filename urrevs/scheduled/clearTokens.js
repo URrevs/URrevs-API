fec9410a06b3f5657c3c1149b0d7d3933ed89b68
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-05-23 02:09:23 +0200

    token revocation system

--- urrevs/scheduled/clearTokens.js
+++ urrevs/scheduled/clearTokens.js
@@ -11,19 +11,19 @@ const config = require("../config");
 let EXPIREATION = (process.env.JWT_EXPIRES_IN || config.JWT_EXPIRES_IN); // must be in the form of "<num_hours>h"
 let tokenDurationHours =  EXPIREATION.substring(0, EXPIREATION.length-1);
 
-let threshDate = new Date();
-threshDate.setHours(threshDate.getHours() - parseInt(tokenDurationHours));
 
 let cronPattern = "00 */" + tokenDurationHours + " * * *";
 
 module.exports = () => {
     cron.schedule(cronPattern, ()=>{
-        console.log("Clearing expired tokens...");
+        console.log("------------------------------------Clearing expired tokens------------------------------------");
+        let threshDate = new Date();
+        threshDate.setHours(threshDate.getHours() - parseInt(tokenDurationHours));
         TOKEN.deleteMany({createdAt: {$lte: threshDate}}).then((resp)=>{
-            console.log("Clearing expired tokens succeeded");
+            console.log("------------------------------------Clearing expired tokens succeeded------------------------------------");
         })
         .catch((err)=>{
-            console.log("Clearing expired tokens FAILED: ", err);
+            console.log("------------------------------------Clearing expired tokens FAILED------------------------------------", err);
         });
     });
 }
