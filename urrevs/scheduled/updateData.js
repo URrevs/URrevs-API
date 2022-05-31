940e0ffb779ab29c191dde5597277af7f1675a50
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-05-31 15:22:48 +0200

    adding default patterns

--- urrevs/scheduled/updateData.js
+++ urrevs/scheduled/updateData.js
@@ -11,9 +11,10 @@ const NEWPHONE = require("../models/newPhone");
 const PHONE = require("../models/phone");
 const PSPECS = require("../models/phoneSpecs");
 const UPDATE = require("../models/update");
+const config = require("../config");
 
 module.exports = ()=>{
-    cron.schedule(process.env.UPDATE_DATA_SCHEDULE_PATTERN , ()=>{ // On the first day of each month at 3:00 AM
+    cron.schedule((process.env.UPDATE_DATA_SCHEDULE_PATTERN || config.UPDATE_DATA_SCHEDULE_PATTERN), ()=>{ // On the first day of each month at 3:00 AM
         updateData.updatePhonesFromSource(COMPANY, PHONE, PSPECS, NEWPHONE, UPDATE, true).then((newItems)=>{
             console.log("End of scheduled update process (SUCCESS)..........................");
         })
