408fdba1d03d7b865ed6367618664cbfef2288c3
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-06-25 12:12:30 +0200

    verify using the api if no matching model names

--- urrevs/utils/mapUaToPhones.js
+++ urrevs/utils/mapUaToPhones.js
@@ -9,7 +9,7 @@ const https = require("https");
 
 const config = require("../config");
 
-module.exports = (useragent, modelName, itemsPerRound, roundNum)=>{
+module.exports = (useragent, modelName, itemsPerRound, roundNum, verify)=>{
     let URL = process.env.USER_AGENT_USER_STACK_API;
     let key = process.env.USER_AGENT_USER_STACK_API_KEY
     let TIMEOUT = parseInt(process.env.TIMEOUT) || config.TIMEOUT;
@@ -53,21 +53,36 @@ module.exports = (useragent, modelName, itemsPerRound, roundNum)=>{
             let updateRes = await PHONE.updateMany({name: {$regex: new RegExp(phoneName, "i")}}, [{$set: {otherNames: {$concat: ["$otherNames", modelName]}}}]);
             
             if(updateRes.modifiedCount > 0){
-                let phonesDocs = await PHONE.find({otherNames: {$regex: modelName, $options: "i"}}, {name: 1, picture: 1, company: 1}).skip((roundNum - 1) * itemsPerRound).limit(itemsPerRound).populate("company", {name: 1});
-                let result = [];
-                
-                for(let phone of phonesDocs){
-                    result.push({
-                        _id: phone._id,
-                        name: phone.name,
-                        picture: phone.picture,
-                        companyId: phone.company._id,
-                        companyName: phone.company.name,
-                        type: "phone"
-                    });
+                if(verify){
+                    let phonesDocs = await PHONE.find({otherNames: {$regex: modelName, $options: "i"}}, {name: 1});
+                    let result = [];
+                    
+                    for(let phone of phonesDocs){
+                        result.push({
+                            _id: phone._id,
+                            name: phone.name,
+                        });
+                    }
+    
+                    return resolve(result);
                 }
-
-                return resolve(result);
+                else{
+                    let phonesDocs = await PHONE.find({otherNames: {$regex: modelName, $options: "i"}}, {name: 1, picture: 1, company: 1}).skip((roundNum - 1) * itemsPerRound).limit(itemsPerRound).populate("company", {name: 1});
+                    let result = [];
+                    
+                    for(let phone of phonesDocs){
+                        result.push({
+                            _id: phone._id,
+                            name: phone.name,
+                            picture: phone.picture,
+                            companyId: phone.company._id,
+                            companyName: phone.company.name,
+                            type: "phone"
+                        });
+                    }
+    
+                    return resolve(result);
+                }
             }
             else{
                 // the phone is not in the database
