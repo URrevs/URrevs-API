4d44e72f665fb3f267cc45be0728dc64c02db39c
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-06-30 04:47:21 +0200

    report phone/company questions answers

--- urrevs/routes/reportsRouter.js
+++ urrevs/routes/reportsRouter.js
@@ -601,7 +601,7 @@ reportRouter.post("/review/phone/:revId/comments/:commentId", cors.cors, rateLim
         console.log("Error from /reports/review/phone/:revId/comments/:commentId: ", err);
         return res.status(500).json({
             success: false,
-            status: "error finding the review or the comment"
+            status: "error finding the review or the comment or the report"
         });
     });
 });
@@ -612,7 +612,7 @@ reportRouter.post("/review/phone/:revId/comments/:commentId", cors.cors, rateLim
 
 
 
-// report a phone review comment
+// report a company review comment
 reportRouter.post("/review/company/:revId/comments/:commentId", cors.cors, rateLimit, authenticate.verifyUser, (req, res, next)=>{
     const reason = req.body.reason;
 
@@ -728,13 +728,266 @@ reportRouter.post("/review/company/:revId/comments/:commentId", cors.cors, rateL
         console.log("Error from /reports/review/company/:revId/comments/:commentId: ", err);
         return res.status(500).json({
             success: false,
-            status: "error finding the review or the comment"
+            status: "error finding the review or the comment or the report"
         });
     });
 });
 
 
 
+
+
+
+
+
+// report a phone question answer
+reportRouter.post("/question/phone/:quesId/answers/:ansId", cors.cors, rateLimit, authenticate.verifyUser, (req, res, next)=>{
+    const reason = req.body.reason;
+
+    if(!reason){
+        return res.status(400).json({
+            success: false,
+            status: "bad request"
+        });
+    }
+
+    if(typeof reason !== "number"){
+        return res.status(400).json({
+            success: false,
+            status: "bad request"
+        });
+    }
+
+    if(!(reason == 1 || reason == 2 || reason == 3 || reason == 4 || reason == 5 || reason == 6)){
+        return res.status(400).json({
+            success: false,
+            status: "bad request"
+        });
+    }
+
+    if(req.body.info){
+        if(typeof req.body.info !== "string"){
+            return res.status(400).json({
+                success: false,
+                status: "bad request"
+            });
+        }
+    }
+
+    // let stringReason = "";
+    // switch(reason){
+    //     case 1: stringReason = "disturbance"; break;
+    //     case 2: stringReason = "violence"; break;
+    //     case 3: stringReason = "harassment"; break;
+    //     case 4: stringReason = "hate"; break;
+    //     case 5: stringReason = "porn"; break;
+    //     case 6: stringReason = "other"; break;
+    // }
+
+
+    if(req.user.blockedFromAnswer){
+        return res.status(403).json({
+            success: false,
+            status: "blocked"
+        });
+    }
+
+
+    let proms = [];
+
+    proms.push(PQUES.findOne({_id: req.params.quesId}));
+    proms.push(PANS.findOne({_id: req.params.ansId, user: {$ne: req.user._id}}, {user: 1}));
+    proms.push(REPORT.findOne({reporter: req.user._id, obj: req.params.ansId, type: "phoneAnswer"}));
+
+    Promise.all(proms)
+    .then((results)=>{
+        let ques = results[0];
+        let answer = results[1];
+        let rep = results[2];
+
+        if(!ques){
+            return res.status(404).json({
+                success: false,
+                status: "parent not found"
+            });
+        }
+
+        if(!answer){
+            return res.status(404).json({
+                success: false,
+                status: "not found or you own it"
+            });
+        }
+
+        if(rep){
+            return res.status(403).json({
+                success: false,
+                status: "already reported"
+            });
+        }
+
+        // create the report document
+            // create the report document
+            REPORT.create({
+                reporter: req.user._id,
+                reportee: answer.user,
+                type: "phoneAnswer",
+                reason: reason,
+                info: req.body.info,
+                obj: req.params.ansId,
+                onModelObj: "pQuesAnswer",
+                parObj: req.params.revId,
+                onModelParObj: "pQues"
+            })
+            .then((r)=>{
+                return res.status(200).json({
+                    success: true
+                });
+            })
+            .catch((err)=>{
+                console.log("Error from /reports/question/company/:quesId/answers/:ansId: ", err);
+                return res.status(500).json({
+                    success: false,
+                    status: "error creating the report"
+                });
+            });
+    })
+    .catch((err)=>{
+        console.log("Error from /reports/question/company/:quesId/answers/:ansId: ", err);
+        return res.status(500).json({
+            success: false,
+            status: "error finding the question or the answer or the report"
+        });
+    });
+});
+
+
+
+
+
+
+
+
+// report a company question answer
+reportRouter.post("/question/company/:quesId/answers/:ansId", cors.cors, rateLimit, authenticate.verifyUser, (req, res, next)=>{
+    const reason = req.body.reason;
+
+    if(!reason){
+        return res.status(400).json({
+            success: false,
+            status: "bad request"
+        });
+    }
+
+    if(typeof reason !== "number"){
+        return res.status(400).json({
+            success: false,
+            status: "bad request"
+        });
+    }
+
+    if(!(reason == 1 || reason == 2 || reason == 3 || reason == 4 || reason == 5 || reason == 6)){
+        return res.status(400).json({
+            success: false,
+            status: "bad request"
+        });
+    }
+
+    if(req.body.info){
+        if(typeof req.body.info !== "string"){
+            return res.status(400).json({
+                success: false,
+                status: "bad request"
+            });
+        }
+    }
+
+    // let stringReason = "";
+    // switch(reason){
+    //     case 1: stringReason = "disturbance"; break;
+    //     case 2: stringReason = "violence"; break;
+    //     case 3: stringReason = "harassment"; break;
+    //     case 4: stringReason = "hate"; break;
+    //     case 5: stringReason = "porn"; break;
+    //     case 6: stringReason = "other"; break;
+    // }
+
+
+    if(req.user.blockedFromAnswer){
+        return res.status(403).json({
+            success: false,
+            status: "blocked"
+        });
+    }
+
+
+    let proms = [];
+
+    proms.push(CQUES.findOne({_id: req.params.quesId}));
+    proms.push(CANS.findOne({_id: req.params.ansId, user: {$ne: req.user._id}}, {user: 1}));
+    proms.push(REPORT.findOne({reporter: req.user._id, obj: req.params.ansId, type: "companyAnswer"}));
+
+    Promise.all(proms)
+    .then((results)=>{
+        let ques = results[0];
+        let answer = results[1];
+        let rep = results[2];
+
+        if(!ques){
+            return res.status(404).json({
+                success: false,
+                status: "parent not found"
+            });
+        }
+
+        if(!answer){
+            return res.status(404).json({
+                success: false,
+                status: "not found or you own it"
+            });
+        }
+
+        if(rep){
+            return res.status(403).json({
+                success: false,
+                status: "already reported"
+            });
+        }
+
+        // create the report document
+            // create the report document
+            REPORT.create({
+                reporter: req.user._id,
+                reportee: answer.user,
+                type: "companyAnswer",
+                reason: reason,
+                info: req.body.info,
+                obj: req.params.ansId,
+                onModelObj: "cQuesAnswer",
+                parObj: req.params.revId,
+                onModelParObj: "cQues"
+            })
+            .then((r)=>{
+                return res.status(200).json({
+                    success: true
+                });
+            })
+            .catch((err)=>{
+                console.log("Error from /reports/question/company/:quesId/answers/:ansId: ", err);
+                return res.status(500).json({
+                    success: false,
+                    status: "error creating the report"
+                });
+            });
+    })
+    .catch((err)=>{
+        console.log("Error from /reports/question/company/:quesId/answers/:ansId: ", err);
+        return res.status(500).json({
+            success: false,
+            status: "error finding the question or the answer or the report"
+        });
+    });
+});
 
 
 
