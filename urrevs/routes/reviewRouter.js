e2a0ecb119c67abf0e917c590171fa0204e8fb11
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-07-17 16:19:00 +0200

    revert past date check

--- urrevs/routes/reviewRouter.js
+++ urrevs/routes/reviewRouter.js
@@ -263,14 +263,21 @@ reviewRouter.post("/phone", cors.cors, rateLimit, authenticate.verifyUser, async
     let ownDate = new Date(Date.parse(ownedDate));
     let relDate = new Date(Date.parse(phone.releaseDate))
 
-    if(ownDate.getYear() < relDate.getYear()){
-      return res.status(400).json({
-        success: false,
-        status: "past date"
-      });
-    }
+    // if(ownDate.getYear() < relDate.getYear()){
+    //   return res.status(400).json({
+    //     success: false,
+    //     status: "past date"
+    //   });
+    // }
 
-    if(ownDate.getMonth() < relDate.getMonth()){
+    // if(ownDate.getMonth() < relDate.getMonth()){
+    //   return res.status(400).json({
+    //     success: false,
+    //     status: "past date"
+    //   });
+    // }
+
+    if(ownDate < relDate){
       return res.status(400).json({
         success: false,
         status: "past date"
